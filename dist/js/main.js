f3d4068061c9a03eb98c9740ec51163b3790cc61
Proshka-dev <proshka_s@mail.ru>
2025-02-01 00:42:46 +0700

    Сделал разделение дат на 2 инпута с выбранного диапазона

--- dist/js/main.js
+++ dist/js/main.js
@@ -6,8 +6,46 @@ console.log('Привет из TS файла');
 // @ts-ignore
 if (AirDatepicker) {
     // @ts-ignore
-    var AirDatepicker1 = new AirDatepicker('#inputdate1');
+    var AirDatepicker1 = new AirDatepicker('#inputdate1', {
+        range: true,
+        multipleDatesSeparator: ' - ',
+        buttons: ['clear'],
+        onSelect: function (_a) {
+            var date = _a.date, formattedDate = _a.formattedDate, datepicker = _a.datepicker;
+            var input1 = document.querySelector('#inputdate1');
+            var input2 = document.querySelector('#inputdate2');
+            if (formattedDate[0]) {
+                input1.value = formattedDate[0];
+            }
+            ;
+            if (formattedDate[1]) {
+                input2.value = formattedDate[1];
+            }
+            else {
+                input2.value = '';
+            }
+        }
+    });
     // @ts-ignore
-    var AirDatepicker2 = new AirDatepicker('#inputdate2');
+    var AirDatepicker2 = new AirDatepicker('#inputdate2', {
+        range: true,
+        multipleDatesSeparator: ' - ',
+        buttons: ['clear'],
+        onSelect: function (_a) {
+            var date = _a.date, formattedDate = _a.formattedDate, datepicker = _a.datepicker;
+            var input1 = document.querySelector('#inputdate1');
+            var input2 = document.querySelector('#inputdate2');
+            if (formattedDate[0]) {
+                input1.value = formattedDate[0];
+            }
+            ;
+            if (formattedDate[1]) {
+                input2.value = formattedDate[1];
+            }
+            else {
+                input2.value = '';
+            }
+        }
+    });
 }
 //new AirDatepicker('#inputdate1');
--- src/pages/main/main.ts
+++ src/pages/main/main.ts
@@ -1,20 +1,42 @@
 //import * as flsFunctions from "./modules/functions.js"
-
 //flsFunctions.isWebp();
-
-console.log('Привет из TS файла');
-
 
 //- Подключение календаря
 //import AirDatepicker from 'air-datepicker';
-
 // @ts-ignore
 if (AirDatepicker) {
     // @ts-ignore
-    const AirDatepicker1 = new AirDatepicker('#inputdate1');
+    const AirDatepicker1 = new AirDatepicker('#inputdate1', {
+        range: true,
+        multipleDatesSeparator: ' - ',
+        buttons: ['clear'],
+        onSelect: function ({ date, formattedDate, datepicker }) {
+            const input1 = document.querySelector('#inputdate1');
+            const input2 = document.querySelector('#inputdate2');
+            if (formattedDate[0]) { input1.value = formattedDate[0] };
+            if (formattedDate[1]) {
+                input2.value = formattedDate[1]
+            } else {
+                input2.value = '';
+            }
+        }
+    });
     // @ts-ignore
-    const AirDatepicker2 = new AirDatepicker('#inputdate2');
+    const AirDatepicker2 = new AirDatepicker('#inputdate2', {
+        range: true,
+        multipleDatesSeparator: ' - ',
+        buttons: ['clear'],
+        onSelect: function ({ date, formattedDate, datepicker }) {
+            const input1 = document.querySelector('#inputdate1');
+            const input2 = document.querySelector('#inputdate2');
+            if (formattedDate[0]) { input1.value = formattedDate[0] };
+            if (formattedDate[1]) {
+                input2.value = formattedDate[1]
+            } else {
+                input2.value = '';
+            }
+        }
+    });
 
 }
-//new AirDatepicker('#inputdate1');
 
