d5dec999adda7c5fc005b9746eb91a50d96f9161
Proshka-dev <proshka_s@mail.ru>
2025-02-06 13:39:14 +0700

    Исправил dropdown-date-split

--- src/modules/dropdown-date-filter/dropdown-date-filter.ts
+++ src/modules/dropdown-date-filter/dropdown-date-filter.ts
@@ -1,39 +1,37 @@
 // Импорт модуля календаря
-//import AirDatepicker from 'air-datepicker';
+import AirDatepicker from 'air-datepicker';
 
 // Кастомные кнопки для календаря
-// let dpButtonApply = {
-//     content: 'Применить',
-//     className: 'dp-custom-button-apply',
-//     onClick: (dp: { hide: () => void }) => {
-//         if ('hide' in dp) {
-//             dp.hide();
-//         }
-//     }
-// }
+let dpButtonApply = {
+    content: 'Применить',
+    className: 'dp-custom-button-apply',
+    onClick: (dp: { hide: () => void }) => {
+        if ('hide' in dp) {
+            dp.hide();
+        }
+    }
+}
 
-// let dpButtonClear = {
-//     content: 'Очистить',
-//     className: 'dp-custom-button-clear',
-//     onClick: (dp: { clear: () => void, update: () => void }) => {
-//         if ('hide' in dp) {
-//             dp.clear();
-//             dp.update();
-//         }
-//     }
-// }
+let dpButtonClear = {
+    content: 'Очистить',
+    className: 'dp-custom-button-clear',
+    onClick: (dp: { clear: () => void, update: () => void }) => {
+        if ('hide' in dp) {
+            dp.clear();
+            dp.update();
+        }
+    }
+}
 
 
-//- Подключение календаря
-//import AirDatepicker from 'air-datepicker';
-// @ts-ignore
-// if (AirDatepicker) {
-//     // @ts-ignore
-//     const AirDatepicker3 = new AirDatepicker('#inputfilter1', {
-//         range: true,
-//         multipleDatesSeparator: ' - ',
-//         buttons: [dpButtonClear, dpButtonApply],
-//         //        inline: true,
-//         dateFormat: 'd MMM'
-//     });
-// }
+//@ts-ignore
+if (AirDatepicker) {
+    // @ts-ignore
+    const AirDatepicker2 = new AirDatepicker('#inputfilter1', {
+        range: true,
+        multipleDatesSeparator: ' - ',
+        buttons: [dpButtonClear, dpButtonApply],
+        //        inline: true,
+        dateFormat: 'd MMM'
+    });
+}
