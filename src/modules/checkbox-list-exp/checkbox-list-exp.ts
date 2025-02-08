7074e88451d60d4d6ac353b97e55800bdd8332a5
Proshka-dev <proshka_s@mail.ru>
2025-02-08 16:15:44 +0700

    Доделал Checkbox list

--- src/modules/checkbox-list-exp/checkbox-list-exp.ts
+++ src/modules/checkbox-list-exp/checkbox-list-exp.ts
@@ -21,55 +21,22 @@ const openAndCloseActions = function (params: IOpenCloseParams) {
         //добавляем визуальный эффект, аналогичный фокусу на кнопке
         buttonElement.classList.add(buttonActiveClass);
     });
-
-    //скрываем по клику снаружи кнопки
-    const myCheckboxListWrapper = document.querySelector('.checkbox-list');
-    console.log('myCheckboxListWrapper: ', myCheckboxListWrapper);
-    // if ((e.target !== buttonElement) && (e.target !== listElement)) {
-
-    document.addEventListener('click', function (e) {
-
-        const target = e.target as HTMLElement;
-
-        if ((target !== buttonElement) && (target.parentElement.parentElement !== listElement)) {
-            //снимаем визуальное отображение фокуса
-            buttonElement.classList.remove(buttonActiveClass);
-            //скрываем список
-            listElement.classList.remove(listVisibleClass);
-            //убираем кнопке атрибут, по которому менняется стрелка
-            buttonElement.classList.remove(buttonListOpnenedClass);
-        }
-        //e.stopPropagation;
-    })
-
-    //скрываем нажатию на Tab или Escape
-    document.addEventListener('keydown', function (e) {
-        if (e.key === 'Tab' || e.key === 'Escape') {
-            //снимаем визуальное отображение фокуса
-            buttonElement.classList.remove(buttonActiveClass);
-            //скрываем список
-            listElement.classList.remove(listVisibleClass);
-            //убираем кнопке атрибут, по которому менняется стрелка
-            buttonElement.classList.remove(buttonListOpnenedClass);
-        }
-    })
-
 };
 
 
 // *****************************************************************************
 // ************** Выборка всех .checkbox-list и обработка каждого **************
 // *****************************************************************************
-document.querySelectorAll('.checkbox-list').forEach(function (checkboxListWrapper) {
+document.querySelectorAll('.checkbox-list-exp').forEach(function (checkboxListWrapper) {
 
     // селекторы
-    const checkboxListButton = checkboxListWrapper.querySelector('.checkbox-list__button');
-    const checkboxListItems = checkboxListWrapper.querySelector('.checkbox-list__items');
+    const checkboxListButton = checkboxListWrapper.querySelector('.checkbox-list-exp__button');
+    const checkboxListItems = checkboxListWrapper.querySelector('.checkbox-list-exp__items');
 
     // классы элементов
-    const classButtonListOpened = 'checkbox-list__button_listopened';
-    const classButtonActive = 'checkbox-list__button_active';
-    const classListVisible = 'checkbox-list__items_visible';
+    const classButtonListOpened = 'checkbox-list-exp__button_listopened';
+    const classButtonActive = 'checkbox-list-exp__button_active';
+    const classListVisible = 'checkbox-list-exp__items_visible';
 
     // Вызов функции назначения обработчиков для открытия и закрытия списка
     openAndCloseActions({
