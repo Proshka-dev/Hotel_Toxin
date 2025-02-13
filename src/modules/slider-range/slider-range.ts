2994e7dd4157c8dc7d0431458bbcde713b6cc9cc
Proshka-dev <proshka_s@mail.ru>
2025-02-13 19:03:48 +0700

    сделал buttons: dark, light, clear

--- src/modules/slider-range/slider-range.ts
+++ src/modules/slider-range/slider-range.ts
@@ -101,33 +101,3 @@ document.querySelectorAll('.slider-range').forEach(function (sliderRangeWrapper:
     });
 
 });
-
-
-
-
-
-
-
-// *****************************************************************************
-// ************** Выборка всех .slider-range и обработка каждого ***************
-// *****************************************************************************
-// document.querySelectorAll('.button-like').forEach(function (buttonLike: HTMLElement) {
-//     buttonLike.addEventListener('click', function () {
-//         let value = Number(buttonLike.dataset.value);
-//         if (buttonLike.classList.contains('button-like_active')) {
-//             // Если была активна
-//             value = value - 1;
-//         } else {
-//             // Если была неактивна
-//             value = value + 1;
-//         };
-
-//         // Изменяем значение data-value
-//         buttonLike.dataset.value = String(value);
-//         // Изменяем отображаемый текст
-//         buttonLike.querySelector('.button-like__text').innerHTML = String(value);
-//         // Переключаем класс button-like_active
-//         buttonLike.classList.toggle('button-like_active');
-
-//     });
-// });
