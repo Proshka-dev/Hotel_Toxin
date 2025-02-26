a63220eb2bd2d777afba3f222eb8a41c37fb2b2e
Proshka-dev <proshka_s@mail.ru>
2025-02-26 16:58:47 +0700

    footer: закончил медиазапрос

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -1,5 +1,7 @@
 // Копирование
+import gulpCopy from 'gulp-copy'; // переименование
+
 export const fonts = () => {
-    return app.gulp.src(app.path.src.fonts)
+    return app.gulp.src(app.path.src.fonts, { encoding: false })
         .pipe(app.gulp.dest(app.path.build.fonts))
 }
