d4e667c327d6576bf1d5649a8ed4bb16363b10d4
Proshka-dev <proshka_s@mail.ru>
2025-03-06 19:26:25 +0700

    button-like: автонастройка ширины

--- src/modules/button-like/button-like.ts
+++ src/modules/button-like/button-like.ts
@@ -1,27 +1,48 @@
-// *****************************************************************************
-// *****************************************************************************
-const buttonLikeActivate = () => {
-    /** Выборка всех.button - like и обработка каждого */
-    document.querySelectorAll('.button-like').forEach(function (buttonLike: HTMLElement) {
-        buttonLike.addEventListener('click', function () {
-            let value = Number(buttonLike.dataset.value);
-            if (buttonLike.classList.contains('button-like_active')) {
-                // Если была активна
-                value = value - 1;
-            } else {
-                // Если была неактивна    
-                value = value + 1;
-            };
+// *********************************************************************************
+//                              Функции
+// *********************************************************************************
+const setWidthDependingOnContent = (buttonLike: HTMLElement) => {
+	const textElement = buttonLike.querySelector('.button-like__text') as HTMLElement;
+	if (!textElement) return;
+	const numOfSimbols = textElement.innerText.length;
 
-            // Изменяем значение data-value
-            buttonLike.dataset.value = String(value);
-            // Изменяем отображаемый текст
-            buttonLike.querySelector('.button-like__text').innerHTML = String(value);
-            // Переключаем класс button-like_active
-            buttonLike.classList.toggle('button-like_active');
-
-        });
-    });
+	if (numOfSimbols > 2) {
+		textElement.style.minWidth = String(13.4 + (numOfSimbols - 2) * 6.63) + 'px';
+	};
 };
 
+// *********************************************************************************
+//                              Основная часть
+// *********************************************************************************
+const buttonLikeActivate = () => {
+	/** Выборка всех.button - like и обработка каждого */
+	document.querySelectorAll('.button-like').forEach(function (buttonLike: HTMLElement) {
+		/** Обработчик события click */
+		buttonLike.addEventListener('click', function () {
+			let value = Number(buttonLike.dataset.value);
+			if (buttonLike.classList.contains('button-like_active')) {
+				// Если была активна
+				value = value - 1;
+			} else {
+				// Если была неактивна    
+				value = value + 1;
+			};
+
+			// Изменяем значение data-value
+			buttonLike.dataset.value = String(value);
+			// Изменяем отображаемый текст
+			buttonLike.querySelector('.button-like__text').innerHTML = String(value);
+			// Переключаем класс button-like_active
+			buttonLike.classList.toggle('button-like_active');
+		});
+
+		/** Установка ширины блока с подстройкой под содержимое */
+		setWidthDependingOnContent(buttonLike);
+
+	});
+};
+
+// *********************************************************************************
+//                              Экспорт
+// *********************************************************************************
 export { buttonLikeActivate }
